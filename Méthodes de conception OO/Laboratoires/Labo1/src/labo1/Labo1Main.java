f6be7c29beddcdf936011dea74775ef34870fcf5
davidyoan <davidyoan07@gmail.com>
2019-09-10 11:27:41 -0400

    J'ai encore arrangé les osti d'accents

--- Méthodes de conception OO/Laboratoires/Labo1/src/labo1/Labo1Main.java
+++ Méthodes de conception OO/Laboratoires/Labo1/src/labo1/Labo1Main.java
@@ -18,7 +18,7 @@ public class Labo1Main extends JFrame {
 	
 	/*
 	 * Cette belle fonction initialise le dessin. Elle lit un Point
-	 * au clavier ou ï¿½ partir du fichier
+	 * au clavier ou à partir du fichier
 	 */
 	
 	private Point initDessin(Scanner reader){
@@ -38,9 +38,9 @@ public class Labo1Main extends JFrame {
 	
 	/*
 	 * Initialisation de la classe de test.
-	 * On obtient un nom de fichier des paramï¿½tres d'exï¿½cution.
+	 * On obtient un nom de fichier des paramètres d'exécution.
 	 * Si on n'a pas de parametre ou un fichier inexistant, on utilise
-	 * le clavier comme flot d'entrï¿½e.
+	 * le clavier comme flot d'entrée.
 	 */
 	
 	public Labo1Main(String[] args) {	
@@ -57,13 +57,13 @@ public class Labo1Main extends JFrame {
 		}
 
 		catch (FileNotFoundException e) {
-			System.out.println("Fichier non trouvï¿½...");
+			System.out.println("Fichier non trouvé...");
 			reader = new Scanner(new InputStreamReader(System.in));
 		}
     	}
 		else
 		{
-			System.out.println("Aucun fichier en paramï¿½tre. Entrez les donnï¿½es dans la console.");
+			System.out.println("Aucun fichier en paramètre. Entrez les données dans la console.");
 			reader = new Scanner(System.in);
 		}
 
@@ -72,11 +72,11 @@ public class Labo1Main extends JFrame {
 
 	
 	/*
-	 * Initialisation de la fenï¿½tre.
+	 * Initialisation de la fenêtre.
 	 */
     private void initUI(Scanner reader) {
     	
-    	//Obtention d'un point ï¿½ partir du fichier ou du clavier
+    	//Obtention d'un point à partir du fichier ou du clavier
     	Point lepoint = initDessin(reader);    	
 
     	//Affichage sur la console de la valeur du point
@@ -85,10 +85,10 @@ public class Labo1Main extends JFrame {
     	//Panneau qui sert de surface pour dessiner.
         PanneauDessin surface = new PanneauDessin(lepoint);
         
-        //La surface de dessin est placï¿½e dans la fenï¿½tre.
+        //La surface de dessin est placée dans la fenêtre.
         add(surface);
         
-        //Configurations relatives ï¿½ la fenï¿½tre.
+        //Configurations relatives à la fenêtre.
         setTitle("Labo 1");
         setSize(800, 600);
         setLocationRelativeTo(null);
