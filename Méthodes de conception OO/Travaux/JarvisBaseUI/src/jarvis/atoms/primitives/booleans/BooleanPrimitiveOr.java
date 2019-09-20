6584232a3b92a96b266be50d37c8187f35ab3489
davidyoan <davidyoan07@gmail.com>
2019-09-20 13:14:03 -0400

    salug

---
+++ Méthodes de conception OO/Travaux/JarvisBaseUI/src/jarvis/atoms/primitives/booleans/BooleanPrimitiveOr.java
@@ -0,0 +1,33 @@
+package jarvis.atoms.primitives.booleans;
+
+import java.util.ArrayList;
+
+import jarvis.atoms.AbstractAtom;
+import jarvis.atoms.BoolAtom;
+import jarvis.atoms.ObjectAtom;
+import jarvis.interpreter.JarvisInterpreter;
+
+public class BooleanPrimitiveOr extends BooleanPrimitiveOperation{
+	@Override
+	public String makeKey() {
+
+		return "BooleanPrimitiveOr";
+	}
+
+	@Override
+	protected AbstractAtom calculateResult(JarvisInterpreter ji, BoolAtom val1, BoolAtom val2) {
+
+		// C'est ici que l'op�ration r�elle a lieu
+		boolean result = val1.getValue() || val2.getValue();
+		
+
+
+		// Ici, construit un objet int manuellement
+		// � noter, on retourne un objet de type bool, pas un atome de type
+		// bool.
+		ArrayList<AbstractAtom> data = new ArrayList<AbstractAtom>();
+		data.add(new BoolAtom(result));
+
+		return new ObjectAtom(((ObjectAtom) ji.getEnvironment().get("bool")), data, ji);
+	}
+}
