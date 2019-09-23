2ecf7cbc91bb2b424f893a3ca1597853b311f1da
davidyoan <davidyoan07@gmail.com>
2019-09-23 16:32:55 -0400

    Modification du test

--- Méthodes de conception OO/Laboratoires/Labo2/labo2base/src/tests/TestVecteur.java
+++ Méthodes de conception OO/Laboratoires/Labo2/labo2base/src/tests/TestVecteur.java
@@ -2,13 +2,53 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import labo2.Vecteur;
 
 public class TestVecteur {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+
+	double[] s1 = { 1, 2, 3, 14 };			
+	Vecteur l1 = new Vecteur(s1);	
+	
+	
+	@Before
+	public void setup(){
+		
+				
+		l1 = new Vecteur(s1);	
+		
 	}
+	@Test
+	public void testToString() {
+
+		double[] s1l1 = { 1, 2, 3, 14 };		
+
+		Vecteur l1 = new Vecteur(s1l1);	
+		
+		String resAttendu = "[1.0 2.0 3.0 14.0]";
+		
+		assertTrue(l1.toString().equals(resAttendu));
+		
+		
+		
+	}
+	
+	@Test
+	public void testEquals() {
+
+		double[] s2 = { 1, 2, 3, 14 };	
+
+		Vecteur l1 = new Vecteur(s1);	
+		
+		Vecteur resAttendu = new Vecteur(s2);	
+		
+		assertEquals(l1,resAttendu);		
+		
+	}
+	
+	
 
 }
