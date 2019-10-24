11e701dc8a9c9dcd91371eaa27fb344ae23fc4cb
davidyoan <davidyoan07@gmail.com>
2019-10-23 23:12:24 -0400

    Reste juste les 2 dernieres fonctions

--- Structure de données/TP3/list2.h
+++ Structure de données/TP3/list2.h
@@ -33,47 +33,85 @@ typename list<TYPE>::cellule* list<TYPE>::erase(cellule* C){
 
 template <typename TYPE>
 class list<TYPE>::reverse_iterator{
-  /*... a effacer et completer ...*/
-  int ATTRIBUT;
+	friend class list<TYPE>;
+private:
+	cellule* POINTEUR;
+public:
+	reverse_iterator(cellule*C = nullptr) :POINTEUR(C) {}
+	TYPE& operator*()const { return POINTEUR->CONTENU; } //*i
+	TYPE* operator->()const { return &(POINTEUR->CONTENU); } //i->
+	iterator& operator++() { POINTEUR = POINTEUR->PREC; return *this; } //++i
+	iterator operator++(int) { iterator it(*this); POINTEUR = POINTEUR->PREC; return it; } //i++
+	iterator& operator--() { POINTEUR = POINTEUR->SUIV; return *this; } //--i
+	iterator operator--(int) { iterator ret(*this); POINTEUR = POINTEUR->SUIV; return ret; } //i--  
+	bool operator==(const iterator& IT)const {
+		return POINTEUR == IT.POINTEUR;
+	}
+	bool operator!=(const iterator& IT)const {
+		return POINTEUR != IT.POINTEUR;
+	}
 };
 
 template <typename TYPE>
 typename list<TYPE>::reverse_iterator list<TYPE>::rbegin(){
-  /*... a effacer et completer ...*/
-  return reverse_iterator();
+  return reverse_iterator(DEBUT->PREC->PREC);
 }
 
 template <typename TYPE>
 typename list<TYPE>::reverse_iterator list<TYPE>::rend(){
-  /*... a effacer et completer ...*/
-  return reverse_iterator();
+  return reverse_iterator(DEBUT);
 }
 
 template <typename TYPE>
 typename list<TYPE>::reverse_iterator list<TYPE>::insert(reverse_iterator i,const TYPE& x){
-  /*... a effacer et completer ...*/
-  return reverse_iterator();
+  return reverse_iterator(insert(i.POINTEUR,x));
 }
 
 template <typename TYPE>
 typename list<TYPE>::reverse_iterator list<TYPE>::erase(reverse_iterator i){
-  /*... a effacer et completer ...*/
-  return reverse_iterator();
+  return reverse_iterator(erase(i.POINTEUR));;
 }
 
 template <typename TYPE>
 void list<TYPE>::operator=(list<TYPE>& L){
-  /*... a completer ...*/
+	clear();
+	iterator it(L.begin());
+	TYPE *c;
+	while (it != L.end())
+	{
+		c = new TYPE(it.POINTEUR->CONTENU);
+		push_back(*c);
+		it++;
+	}
 }
 
 template <typename TYPE>
 void list<TYPE>::resize(size_t N,const TYPE& X){
-  /*... a completer ...*/
+	iterator it(DEBUT);
+	int initSize = SIZE;
+	if(N<SIZE && N>0)
+	{
+		for (int i = 0; i <= N; i++)it++;
+		while (it != end()) it = erase(it);
+	}
+	if (N > SIZE)
+	{
+		for (int i = 0; i < N - initSize; i++)push_back(X);
+	}
 }
 
 template <typename TYPE>
 void list<TYPE>::splice(iterator i,list& L){
-  /*... a completer ...*/
+	i.POINTEUR->PREC->SUIV = L.DEBUT->SUIV;
+	L.DEBUT->SUIV->PREC = i.POINTEUR->PREC;
+	i.POINTEUR->PREC = L.DEBUT->PREC->PREC;
+	L.DEBUT->PREC->PREC->SUIV = i.POINTEUR;
+
+	L.DEBUT->SUIV = L.DEBUT->PREC;
+	L.DEBUT->SUIV->PREC = L.DEBUT;
+	SIZE += L.SIZE;
+	L.SIZE = 0;
+  
 }
 
 template <typename TYPE>
--- Structure de données/TP3/main.cpp
+++ Structure de données/TP3/main.cpp
@@ -11,12 +11,43 @@ int main()
 	test.push_back(5);
 	test.push_front(2);
 	test.push_front(0);
+
 	list<int>::iterator it(test.begin());
 	it++;
 	test.insert(it, 1);
+
 	it = test.begin();
 	test.erase(it);
 	test.afficher();
+
+
+	list<int> test2;
+	test2.push_back(33);
+	test2.push_back(44);
+	test2.push_back(55);
+	test2.push_back(66);
+	test2.push_back(77);
+	test2.push_back(88);
+	test2.push_back(99);
+	test = test2;
+	test.afficher();
+
+
+
+	test.resize(3,4);
+	test.afficher();
+	test.resize(5,4);
+	test.afficher();
+
+	list<int>::iterator it2(test.begin());
+	it2++;
+	it2++;
+	test.splice(it2, test2);
+	test.afficher();
+	test2.afficher();
+
+
+
 	system("PAUSE");
 
 
